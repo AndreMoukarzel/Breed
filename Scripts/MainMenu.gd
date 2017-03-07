6a94ad34737016e4241cd744fdb5116945bba329
Detril <andremoukarzel@hotmail.com>
2017-03-07 11:20:24 -0300

    Created base for most GUI

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,16 @@
+
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_New_Game_pressed():
+	get_node("/root/global").goto_scene("res://scenes/jaiminho.tscn")
+
+
+func _on_Load_Game_pressed():
+	var savegame = File.new()
+	if !savegame.file_exists("user://savegame.save"):
+		return #Error!  We don't have a save to load
