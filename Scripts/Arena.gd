ef77a4d6ce40f95d9b622393ba8ac1d637d0ebf4
Luiz Girotto <luizfelipe.g@hotmail.com>
2017-04-10 15:10:30 -0300

    Begin arena

--- Scripts/Arena.gd
+++ Scripts/Arena.gd
@@ -1,9 +1,8 @@
 
 extends Control
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var PageAmount = 8
+var BoxColumns = 2
 
 func _ready():
 	print("Arena")
@@ -15,3 +14,31 @@ func _on_Back_pressed():
 	self.hide()
 	get_parent().get_node("VBox").show()
 	get_parent().get_node("FarmBackground").show()
+
+func _on_BattleGround_pressed():
+	get_node("BattleMode/MonsterSelect/SelectBox").update_config(global.mon_depo, PageAmount, BoxColumns)
+	get_node("BattleMode/MonsterSelect/SelectBox").generate_members()
+	
+	get_node("VBox").hide()
+	get_node("BattleMode").show()
+
+func _on_Back2_pressed():
+	get_node("VBox").show()
+	get_node("BattleMode").hide()
+	get_node("BattleMode/MonsterSelect/Proceed").set_disabled(true)
+
+func _on_Back3_pressed():
+	get_node("BattleMode/MonsterSelect").show()
+	get_node("BattleMode/RankSelect").hide()
+
+func select_monster(monster, select_box):
+	#test
+	print("ID = ", monster.idn)
+	
+	get_node("BattleMode").selected_id = monster.idn
+	# Enable continue button
+	get_node("BattleMode/MonsterSelect/Proceed").set_disabled(false)
+
+func _on_Proceed_pressed():
+	get_node("BattleMode/MonsterSelect").hide()
+	get_node("BattleMode/RankSelect").show()
---
+++ Scripts/BattleMode.gd
@@ -0,0 +1,63 @@
+
+extends Control
+
+var selected_id = -1
+var comp_depo = []
+
+func _ready():
+	pass
+
+func select_monster(monster, select_box):
+	get_parent().select_monster(monster, select_box)
+
+
+
+func _on_Rank1_pressed():
+	# Vai ter que gerar os monstros para uma batalha de Rank 1,
+	# e começar a representação visual.
+	generate_enemies(1, 5)
+	process_battle(5)
+	pass
+	
+func generate_enemies(rank, number):
+	# Placeholder, deve depender do rank para gerar os monstros.
+	for num in range (0, number):
+		get_parent().get_parent().monster_generate(comp_depo, "nhi", Color(-1, -1, -1), [], [2, 2, 1, 0, 0, 0], 3)
+
+func process_battle(enemy_num):
+	# The battle will have a couple of commands that can be
+	# chosen by the monsters. When a command is issued, the
+	# respective animation should be played, hence why all
+	# commands are separated on their on functions.
+	
+	var mon1
+	var mon2
+	
+	for monster in global.mon_depo:
+		if monster.idn == selected_id:
+			mon1 = monster
+	
+	mon2 = comp_depo[0]
+	
+	# A monster's turn happens when the counter reaches 100
+	var mon1_turn = 0
+	var mon2_turn = 0
+	
+	var mon1_hp = mon1.stats[2] * 3
+	var mon2_hp = mon2.stats[2] * 3
+	
+	while (true):
+		mon1_turn += mon1.stats[1]
+		mon2_turn += mon2.stats[1]
+		if (mon1_turn >= 100 and mon2_turn >= 100):
+			if (randf() < 0.5):
+				# Turno do monster 1 primeiro
+				pass
+			else:
+				# Turno do monster 2 primeiro
+				pass
+		elif (mon1_turn >= 100):
+			pass
+		elif (mon2_turn >= 100):
+			pass
+			
