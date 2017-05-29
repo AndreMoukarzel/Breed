2517984aacc7fe4164754b33973882c9376f7834
Detril <andremoukarzel@hotmail.com>
2017-05-29 13:10:16 -0300

    Reseting variables of dumpster on back

--- Scripts/Dumpster.gd
+++ Scripts/Dumpster.gd
@@ -1,8 +1,10 @@
 
 extends Control
 
-var mon = null
-var cat= null
+var screen = null
+
+var mon = null #selected monster
+var cat = -1 #selected catalyst
 
 func _ready():
 	get_node("Boxes/Mons").update_config(global.mon_depo, 20, 5)
@@ -25,26 +27,41 @@ func select_monster( monster, select_box ):
 func _on_SellMonster_pressed():
 	get_node("VBox").hide()
 	get_node("Boxes/Mons").show()
+	screen = "monster"
 
 
 func _on_SellCatal_pressed():
 	get_node("VBox").hide()
 	get_node("Boxes/Catals").show()
+	screen = "catalyst"
 
 
 func _on_Back_pressed():
 	self.hide()
 	get_parent().get_node("VBox").show()
 	get_parent().get_node("FarmBackground").show()
+	screen = null
+	mon = null
+	cat = -1
 
 
 func _on_BackMon_pressed():
 	get_node("Boxes/Mons").hide()
 	get_node("Boxes/Sell").hide()
 	get_node("VBox").show()
+	mon = null
 
 
 func _on_BackCatal_pressed():
 	get_node("Boxes/Catals").hide()
 	get_node("Boxes/Sell").hide()
 	get_node("VBox").show()
+	cat = -1
+
+
+func _on_Sell_pressed():
+	if (screen == "monster"):
+	
+	elif (screen == "catalyst"):
+		# gib mony
+		re
