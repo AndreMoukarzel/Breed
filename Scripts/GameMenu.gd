804c72d6bbbfcb82cf94f5db1e0b819753ba7c8f
Detril <andremoukarzel@hotmail.com>
2017-08-07 14:30:13 -0300

    Tweaks to dialog font

--- Scripts/GameMenu.gd
+++ Scripts/GameMenu.gd
@@ -2,7 +2,7 @@
 extends Control
 
 func _ready():
-	#global.instace_dialog(self, "System", 0)
+	global.instace_dialog(self, "System", 0)
 	
 	get_node("HUD/Energy").set_max(global.energy)
 	get_node("HUD/Energy").set_value(global.energy)
